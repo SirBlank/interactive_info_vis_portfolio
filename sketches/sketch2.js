2b03fe63f0ec68bbceb0035923f4815f7fef6883
SirBlank <amberyuewu@gmail.com>
2026-02-05 23:38:40 -0800

    add a single moving particle

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -6,13 +6,19 @@ registerSketch('sk2', function (p) {
   let startTime = null;
   let isRunning = false;
 
+  let particleX = 0;
+  let particleY;
+  let minSpeed = 0.2;
+  let maxSpeed = 6;
+
   p.setup = function () {
     p.createCanvas(800, 800);
 
     input = p.createInput('10');
     button = p.createButton('Start Timer');
-
     button.mousePressed(startTimer);
+
+    particleY = p.height / 2;
 
     p.textSize(32);
     p.textAlign(p.CENTER, p.CENTER);
@@ -34,12 +40,18 @@ registerSketch('sk2', function (p) {
       let elapsed = (p.millis() - startTime) / 1000;
       timeLeft = p.max(0, totalTime - elapsed);
 
+      let timeFactor = timeLeft / totalTime;
+      let speed = minSpeed + timeFactor * (maxSpeed - minSpeed);
+
+      particleX += speed;
+
       if (timeLeft === 0) {
         isRunning = false;
       }
     }
 
     p.fill(0);
+    p.circle(particleX, particleY, 20);
     p.text(
       `Time Left: ${timeLeft.toFixed(1)}s`,
       p.width / 2,

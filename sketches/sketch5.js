db47c58dc6942fda418f6dfb08af0af95d019b35
SirBlank <amberyuewu@gmail.com>
2026-02-15 15:43:31 -0800

    load data

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,23 +1,13 @@
-// Example 2
+// HW 5: Narrative Visualization
 registerSketch('sk5', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    hourlyData = p.loadJSON("data/hourly_spotify_data.json")
+    console.log(hourlyData)
+    p.createCanvas(800, 800);
   };
 
   p.draw = function () {
     p.background(250);
-
-    // Corner time display
-    const h = p.hour();
-    const m = p.minute();
-    const s = p.second();
-    const label = p.nf(h, 2) + ':' + p.nf(m, 2) + ':' + p.nf(s, 2);
-
-    p.noStroke();
-    p.fill(20);
-    p.textAlign(p.LEFT, p.TOP);  // change to RIGHT/BOTTOM for other corners
-    p.textSize(50);
-    p.text(label, 12, 10);   // top-left corner
 
   }
 
